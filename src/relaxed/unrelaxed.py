70739a232dea44c0a7ecbff8bf6cde316c5a6dae
Angus Hollands <goosey15@gmail.com>
2022-06-01 15:04:22 +0100

    Fix: import from typing_extensions

--- src/relaxed/unrelaxed.py
+++ src/relaxed/unrelaxed.py
@@ -4,7 +4,9 @@ __all__ = ("relaxed_of", "unrelaxed")
 
 from contextlib import contextmanager
 from functools import wraps
-from typing import Callable, Generator, ParamSpec, TypeVar
+from typing import Callable, Generator, TypeVar
+from typing_extensions import ParamSpec
+
 
 P = ParamSpec("P")
 R = TypeVar("R")
